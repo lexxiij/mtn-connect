e6adf26ac7805c33bd5c63ff67f765c366a15e32
lexxiij <aljdrpamj@yahoo.com>
2025-11-01 23:20:38 -0500

    new calendar, page updates

--- mtnConnect/src/app/event-list/event-list.component.ts
+++ mtnConnect/src/app/event-list/event-list.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+
 
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [DatePipe, RouterLink],
+  imports: [RouterLink, CommonModule, FullCalendarModule],
   templateUrl: './event-list.component.html',
-  styleUrl: './event-list.component.css'
+  styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent {
   events = [
@@ -16,4 +21,27 @@ export class EventListComponent {
     { id: 2, name: 'CDL Training Orientation', date: '2025-11-18' },
   ];
 
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    height: '90%',
+    contentHeight: 'auto',
+    aspectRatio: 1,
+    headerToolbar: {
+      left: 'prev',
+      center: 'title',
+      right: 'next',
+    },
+    plugins: [dayGridPlugin, interactionPlugin],
+    dateClick: (arg) => this.handleDateClick(arg),
+    events: [
+    { title: 'Forklift Training Orientation', date: '2025-11-17' },
+    { title: 'CDL Training Orientation', date: '2025-11-18' },
+  ]
+  };
+
+  handleDateClick(arg: DateClickArg) {
+    alert('date click! ' + arg.dateStr)
+  }
+
 }
+
