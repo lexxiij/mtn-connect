103f1ae39f216e28b422696c8ef06754b2111ef6
lexxiij <aljdrpamj@yahoo.com>
2025-11-01 19:21:39 -0500

    new

--- mtnConnect/src/app/event-list/event-list.component.ts
+++ mtnConnect/src/app/event-list/event-list.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { CalendarComponent } from "../event-calendar/event-calendar.component";
-
-interface Event {
-  id: number;
-  name: string;
-  date: Date;
-}
 
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [DatePipe, RouterLink, CalendarComponent],
+  imports: [DatePipe, RouterLink],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
