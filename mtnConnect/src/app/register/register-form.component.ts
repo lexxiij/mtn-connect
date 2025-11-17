bdf7476bdc2ede5e4965344fe4d26fd00ba271d2
lexxiij <aljdrpamj@yahoo.com>
2025-11-17 17:49:56 -0600

    Updates to registration form

--- mtnConnect/src/app/register/register-form.component.ts
+++ mtnConnect/src/app/register/register-form.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from "@angular/core";
+import { FormsModule, NgForm, } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-registration-form',
+  selector: 'app-register-form',
+  imports: [FormsModule,],
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.css'
+  styleUrls: ['./register-form.component.css'],
 })
-export class RegistrationFormComponent {
 
-}
+export class RegistrationFormComponent {
+    fullName = "";
+    email = "";
+    phone = "";
+    dob = "";
+    education = "";
+    trainingType = "";
+    heardFrom = "";
+    heardOther = "";
+    comments = "";
+    submitted = false;
+    form: any;
+    public missouriCounties: string[] =["Mississippi", "Scott", "New Madrid", "Pemiscot", "Butler","Bollinger", "Dunklin", "Cape Girardeau","Perry","Saint Francois", "Stoddard","Saint Genevieve"];
+
+    submit(form: NgForm) {
+      this.submitted = true;
+      if (form.invalid) return;
+      console.log("[Contact Form]", {
+        name: this.fullName,
+        email: this.email,
+        phone: this.phone,
+        county: this.missouriCounties,
+        trainingType: this.trainingType,
+        heardFrom: this.heardFrom,
+        heardOther: this.heardOther,
+        comments: this.comments,
+      });
+      form.resetForm();
+      this.submitted = false;
+    }
+  }
